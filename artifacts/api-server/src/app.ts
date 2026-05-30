ada00dcacd9f30fd9eb8d47dc0c3b499c13dce68
imamjauhari54-sys <imamjauhari54@guru.sd.belajar.id>
2026-05-30 13:10:52 +0700

    index

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,7 +1,7 @@
 import express, { type Express, type Request, type Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import router from "./routes";
+import router from "./routes/index";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
